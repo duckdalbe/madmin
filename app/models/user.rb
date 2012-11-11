06301c2dc3bec92d690d1253ffcad121e1138e6f
Fup Duck <fup@duckdalbe.org>
2012-11-11 15:41:54 +0100

    Fix *admin?()-methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,14 @@ class User < ActiveRecord::Base
   belongs_to :domain
   attr_accessible :name, :password, :password_confirmation, :domain_id
   validates :name, :presence => true
-  #validates :domain, :inclusion => Domain.find(:all).map(&:id)
   validates :domain, :presence => true
   has_secure_password
 
   def admin?(domain)
-    domain.id == @domain.id && @name == 'postmaster'
+    domain.id == self.domain.id && self.name == 'postmaster'
   end
 
   def superadmin?
-    @domain.name == 'example.org' && @name == 'postmaster'
+    self.domain.name == 'example.org' && self.name == 'postmaster'
   end
 end
