0ea6e9454316d36a6e9b8c1f653f6cb74db91da7
Fup Duck <fup@duckdalbe.org>
2012-11-19 23:45:41 +0100

    Order associations by name.

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,7 +1,7 @@
 class Domain < ActiveRecord::Base
   attr_accessible :name
-  has_many :users, :dependent => :restrict
-  has_many :forwards, :dependent => :restrict
+  has_many :users, :dependent => :restrict, :order => :name
+  has_many :forwards, :dependent => :restrict, :order => :name
 
   def as_json(options={})
     {
