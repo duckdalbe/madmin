8eb33f300791f5d9f4263fff68bc1755bcd66919
Fup Duck <fup@duckdalbe.org>
2012-11-28 21:30:26 +0100

    Rm unused code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,2 @@
 module ApplicationHelper
-  def domains_link
-    if current_user.superadmin?
-      link_to('Domains', domains_url)
-    else
-      ''
-    end
-  end
-
-  def domain_link(domain)
-    if current_user.superadmin? || current_user.admin?(domain)
-      "&rArr;&nbsp;#{link_to(domain.name, domain)}".html_safe
-    else
-      ''
-    end
-  end
 end
