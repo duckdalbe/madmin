f7d082670f1f42e1756739341c4fac2b416faa42
Fup Duck <fup@duckdalbe.org>
2013-01-30 20:55:34 +0100

    Auth-script in ruby.

---
+++ bin/madmin-auth.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+
+# Configuration
+protocol = 'http'
+host = 'localhost'
+port = 3000
+# End of configuration
+
+
+if ARGV.empty?
+  email, pass = $stdin.read.unpack("Z*Z*")
+else
+  if ARGV.first == '-v'
+    verbose = true
+    ARGV.shift
+  else
+    verbose = false
+  end
+  email = ARGV.shift
+  pass = ARGV.shift
+end
+
+name, domain = email.split('@', 2)
+
+
+begin
+  # TODO: validate and escape/encode input.
+  # TODO: POST params
+  resp = open("#{protocol}://#{host}:#{port}/login.json?name=#{name}&domain=#{domain}&password=#{pass}")
+  puts resp.status.last if verbose
+  exit 0
+rescue OpenURI::HTTPError => exc
+  puts exc if verbose
+  exit 1
+rescue => exc
+  # Something went wrong, user will see temporary error.
+  puts exc
+  exit 111
+end
+
