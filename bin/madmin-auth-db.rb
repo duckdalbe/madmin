77bc1f8cac94a80e4e03ddef397fc50b557cc178
Fup Duck <fup@duckdalbe.org>
2013-02-01 22:10:25 +0100

    Auth-script that reads DB directly. Slow.

---
+++ bin/madmin-auth-db.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'sqlite3'
+require 'bcrypt'
+
+dbfile = File.join(%W(#{File.dirname(__FILE__)} .. db development.sqlite3))
+
+name, domain = ARGV.shift.split('@', 2)
+pass = ARGV.shift
+
+pwsql = "select users.password_digest 
+  from users 
+  join domains on users.domain_id=domains.id 
+  where users.name = '%s' and domains.name = '%s'" % [name, domain]
+
+dbcon = SQLite3::Database.new(dbfile)
+pwhash = dbcon.get_first_value(pwsql)
+if pwhash.nil?
+  puts false
+  exit 1
+end
+bcrypt_pass = BCrypt::Password.new(pwhash)
+
+if bcrypt_pass == pass
+  puts 'ok'
+  exit 0
+else
+  puts 'failed'
+  exit 1
+end
