df6790d1a34f96178b78322155f27332b55ecfb2
Fup Duck <fup@duckdalbe.org>
2013-03-04 21:54:29 +0100

    Fix checkpassword-interface of madmin-auth.sh.

--- bin/madmin-auth.sh
+++ bin/madmin-auth.sh
@@ -1,31 +1,33 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 
 protocol=http
 host=localhost
 port=3000
 
-if [ $# -ne 2 ]; then
-  echo "Usage: $(basename $0) email password"
-  exit 1
+if [ $# -ne 0 ]; then
+  echo "Usage: $(basename $0) 3< 'username\0password\0'"
+  exit 2
 fi
 
-name="${1%@*}"
-domain="${1#*@}"
-pass="$2"
+exec 3<&0
+read -t 1 -r user pass
+
+name="${user%@*}"
+domain="${user#*@}"
 url="$protocol://$host:$port/login.json"
 
-status=$(curl -i -s -S --data-urlencode "name=$name" --data-urlencode "domain=$domain" --data-urlencode "password=$pass" $url | awk '/^HTTP/ {print $2}')
+response=$(curl -i -s -S --data-urlencode "name=$name" --data-urlencode "domain=$domain" --data-urlencode "password=$pass" $url | awk '/^HTTP/ {print $2}')
 
-case $status in 
+case $response in
   3*|2*)
-    echo 'ok'
+    # ok
     exit 0
     ;;
   4*|5*)
-    echo 'failed'
+    # fail
     exit 1
     ;;
   *)
-    echo $status
-    exit 1
+    # something unknown is wrong, temp error
+    exit 111
 esac
