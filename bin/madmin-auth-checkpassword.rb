1b61efe4c089e45786f16924f24b4729a8d7fbb0
Fup Duck <fup@duckdalbe.org>
2014-09-12 12:10:58 +0200

    Improve checkpassword-auth-script

--- bin/madmin-auth-checkpassword.rb
+++ bin/madmin-auth-checkpassword.rb
@@ -1,39 +1,72 @@
 #!/usr/bin/env ruby
-# -*- encoding : utf-8 -*-
+
+## Config
+
+UserHome = '/tmp/'
+UserUid  = 111
+UserGid  = 8
+
+## Setup
 
 require 'mysql2'
 require 'bcrypt'
 require 'yaml'
 
-dbfile = File.join(File.dirname(__FILE__), '..', 'config', 'database.yml')
-dbconf = YAML.load(File.read(dbfile))['production']
-dbh = Mysql2::Client.new(
-      host: dbconf['host'],
-      username: dbconf['username'],
-      password: dbconf['password'],
-      database: dbconf['database']
-    )
-
-name = ENV['AUTH_USERNAME']
-domain = ENV['AUTH_DOMAIN']
-password = ENV['AUTH_PASSWORD']
-
-sql = "select users.password_digest 
-  from users 
-  join domains on users.domain_id=domains.id 
-  where users.name = '%s' and domains.name = '%s'" % [name, domain]
-
-if (res = dbh.query(sql).first).nil?
-  #puts false
-  exit 1
+def fetch_user(email)
+  name, domain = email.split('@', 2)
+  sql = "select users.password_digest 
+    from users 
+    join domains on users.domain_id=domains.id 
+    where users.name = '%s' and domains.name = '%s'" % [name, domain]
+  dbfile = File.join(File.dirname(__FILE__), '..', 'config', 'database.yml')
+  dbconf = YAML.load(File.read(dbfile))['production']
+  dbh = Mysql2::Client.new(
+        host: dbconf['host'],
+        username: dbconf['username'],
+        password: dbconf['password'],
+        database: dbconf['database']
+      )
+  dbh.query(sql).first
 end
 
-bcrypt_pass = BCrypt::Password.new(res['password_digest'])
-
-if bcrypt_pass == password
-  # OK
-  exit 0
-else
-  # Failed
-  exit 1
+def checkpassword(user, password)
+  # TODO: check old hash, migration from it
+  BCrypt::Password.new(user['password_digest']) == password
 end
+
+def dovecot_env(email)
+  vars = {
+    USER: email,
+    HOME: UserHome,
+    EXTRA: 'userdb_uid userdb_gid',
+    userdb_uid: UserUid,
+    userdb_gid: UserGid
+  }
+
+  vars.map do |k,v|
+    "#{k}='#{v}'"
+  end.join(' ')
+end
+
+### Start script
+
+dovecot_reply_arg = ARGV[0]
+InternalAuthError = 111
+AuthError         = 1
+
+begin
+  IO.new(3, 'r') do |io|
+    @email, @password = io.read.unpack("Z*Z*")
+  end
+
+  user = fetch_user(@email)
+
+  if user.nil? || ! checkpassword(user, @password)
+    exit AuthError
+  end
+rescue
+  # TODO: log errors
+  exit InternalAuthError
+end
+
+exec "#{dovecot_env(@email)} #{dovecot_reply_arg}"
